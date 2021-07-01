792684d4ef20d1b61adb5d4f0a7d5dde37f193b4
mynameduc <86762202+mynameduc@users.noreply.github.com>
2021-07-01 12:58:46 +0700

    Update config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ const CONFIG = {
     btnIntro: 'hihi',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích cậuuu 😙 ',
+    btnYes: 'Vẫn cứ là thích cậuuuu 😙 ',
     btnNo: 'Không, nhé cậu à :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
